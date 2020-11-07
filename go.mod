fefd9b8cd7ed5a37e78b8dac9e18c13cccf61397
Sarawuth Pimsai <sarawuth.pimsai@gmail.com>
2020-11-07 21:41:29 +0700

    Add Cors

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.15
 
 require (
 	github.com/go-chi/chi v4.1.2+incompatible
+	github.com/go-chi/cors v1.1.1
 	github.com/go-sql-driver/mysql v1.5.0
 )
--- routes/router.go
+++ routes/router.go
@@ -5,12 +5,21 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 )
 
 //InitialRouter ...
 func InitialRouter() http.Handler {
 	var r *chi.Mux
 	r = chi.NewRouter()
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:8080"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
 	r.Route("/api", func(r chi.Router) {
 		r.Mount("/users", api.UserAPI())
 		r.Mount("/authors", api.AuthorAPI())
