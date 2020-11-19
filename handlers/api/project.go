48cd6bbb60c9c98eb9923622ec5df2039f96a5ae
Sarawuth Pimsai <sarawuth.pimsai@gmail.com>
2020-11-19 19:58:54 +0700

    Coding Project handler

---
+++ handlers/api/project.go
@@ -0,0 +1,174 @@
+package api
+
+import (
+	"costperfect/backend/handlers/api/input"
+	"costperfect/backend/models"
+	"costperfect/backend/stores/mariadb"
+	"encoding/json"
+	"io"
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"gopkg.in/go-playground/validator.v9"
+)
+
+//Project ...
+type Project struct {
+	Endpoint string
+}
+
+//NewProject ...
+func NewProject() Project {
+	return Project{Endpoint: "projects"}
+}
+
+//Create ...
+func (p Project) Create(w http.ResponseWriter, r *http.Request) {
+	var input models.Project
+	var mdbProject mariadb.Project
+	var ok bool
+	var err error
+	var lastID int64
+	var res map[string]int64
+
+	if err = json.NewDecoder(r.Body).Decode(&input); err != nil {
+		if err == io.EOF {
+			JSON(w, http.StatusOK, Failure(p.Endpoint, err))
+			return
+		}
+		JSON(w, http.StatusOK, Failure(p.Endpoint, err))
+		return
+	}
+
+	if err = validate.Struct(input); err != nil {
+		if _, ok = err.(*validator.InvalidValidationError); ok {
+			JSON(w, http.StatusOK, Err(p.Endpoint, err))
+			return
+		}
+	}
+	mdbProject = mariadb.NewProject()
+	lastID, err = mdbProject.Create(input)
+	if err != nil {
+		JSON(w, http.StatusOK, Err(p.Endpoint, err))
+		return
+	}
+	res = make(map[string]int64)
+	res["last_id"] = lastID
+	JSON(w, http.StatusOK, Success(p.Endpoint, res))
+}
+
+//Update ...
+func (p Project) Update(w http.ResponseWriter, r *http.Request) {
+	var input input.Project
+	var mProject models.Project
+	var mdbProject mariadb.Project
+	var err error
+	var ok bool
+	var id int64
+
+	id, err = ID64(chi.URLParamFromCtx(r.Context(), "id"))
+	if err != nil {
+		JSON(w, http.StatusOK, Err(p.Endpoint, err))
+		return
+	}
+	if err = json.NewDecoder(r.Body).Decode(&input); err != nil {
+		JSON(w, http.StatusOK, Failure(p.Endpoint, err))
+		return
+	}
+	if err = validate.Struct(input); err != nil {
+		if _, ok = err.(*validator.InvalidValidationError); ok {
+			JSON(w, http.StatusOK, Failure(p.Endpoint, err))
+			return
+		}
+	}
+	mdbProject = mariadb.NewProject()
+	mProject, err = mdbProject.FindByID(id)
+	if err != nil {
+		JSON(w, http.StatusOK, Err(p.Endpoint, err))
+		return
+	}
+	input.Match(&mProject)
+	if err = mdbProject.Update(id, mProject); err != nil {
+		JSON(w, http.StatusOK, Err(p.Endpoint, err))
+		return
+	}
+	JSON(w, http.StatusOK, Success(p.Endpoint, NewEmptyData()))
+}
+
+//Delete ...
+func (p Project) Delete(w http.ResponseWriter, r *http.Request) {
+	var id int64
+	var err error
+	var mdbProject mariadb.Project
+
+	id, err = ID64(chi.URLParamFromCtx(r.Context(), "id"))
+	if err != nil {
+		JSON(w, http.StatusOK, Failure(p.Endpoint, err))
+		return
+	}
+	mdbProject = mariadb.NewProject()
+	if err = mdbProject.Delete(id); err != nil {
+		JSON(w, http.StatusOK, Err(p.Endpoint, err))
+		return
+	}
+	JSON(w, http.StatusOK, Success(p.Endpoint, NewEmptyData()))
+}
+
+//DeleteByIDs ...
+func (p Project) DeleteByIDs(w http.ResponseWriter, r *http.Request) {
+	var ids models.IDs
+	var err error
+	var mdbProject mariadb.Project
+
+	if err = json.NewDecoder(r.Body).Decode(&ids); err != nil {
+		JSON(w, http.StatusOK, Err(p.Endpoint, err))
+		return
+	}
+	mdbProject = mariadb.NewProject()
+	if err = mdbProject.DeleteByIDs(ids.IDs); err != nil {
+		JSON(w, http.StatusOK, Err(p.Endpoint, err))
+		return
+	}
+	JSON(w, http.StatusOK, Success(p.Endpoint, NewEmptyData()))
+}
+
+//Get ...
+func (p Project) Get(w http.ResponseWriter, r *http.Request) {
+	var id int64
+	var err error
+	var mProject models.Project
+	var mdbProject mariadb.Project
+
+	id, err = ID64(chi.URLParamFromCtx(r.Context(), "id"))
+	if err != nil {
+		JSON(w, http.StatusOK, Failure(p.Endpoint, err))
+		return
+	}
+	mdbProject = mariadb.NewProject()
+	mProject, err = mdbProject.FindByID(id)
+	if err != nil {
+		JSON(w, http.StatusOK, Err(p.Endpoint, err))
+		return
+	}
+	if mProject == (models.Project{}) {
+		JSON(w, http.StatusOK, NotFound(p.Endpoint))
+		return
+	}
+	JSON(w, http.StatusOK, Success(p.Endpoint, mProject))
+}
+
+//All ...
+func (p Project) All(w http.ResponseWriter, r *http.Request) {
+	var offset, limit int
+	var mProjects []models.Project
+	var mdbProject mariadb.Project
+	var err error
+
+	mdbProject = mariadb.NewProject()
+	mProjects, err = mdbProject.FindAll(mariadb.WithOffset(offset), mariadb.WithLimit(limit))
+	if err != nil {
+		JSON(w, http.StatusOK, Err(p.Endpoint, err))
+		return
+	}
+	JSON(w, http.StatusOK, Success(p.Endpoint, mProjects))
+}
