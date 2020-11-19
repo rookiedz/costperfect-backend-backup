d12c260fba686a627dc27c69645b2b58ad549b78
Sarawuth Pimsai <sarawuth.pimsai@gmail.com>
2020-11-19 21:46:51 +0700

    Remove created_at, updated_at field

--- models/model.go
+++ models/model.go
@@ -5,8 +5,6 @@ import "time"
 //Model ...
 type Model struct {
 	ID        int64      `json:"id"`
-	Deleted   bool       `json:"-"`
 	CreatedAt *time.Time `json:"-"`
 	UpdatedAt *time.Time `json:"-"`
-	DeletedAt *time.Time `json:"-"`
 }
