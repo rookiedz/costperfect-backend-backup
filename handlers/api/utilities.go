047b78a06e3620d717e202ef20308ad24a4570e2
Sarawuth Pimsai <sarawuth.pimsai@gmail.com>
2020-11-19 21:47:55 +0700

    Remove function

--- handlers/api/utilities.go
+++ handlers/api/utilities.go
@@ -3,9 +3,7 @@ package api
 import (
 	"encoding/json"
 	"errors"
-	"log"
 	"net/http"
-	"reflect"
 	"strconv"
 )
 
@@ -22,40 +20,4 @@ func JSON(w http.ResponseWriter, status int, entry interface{}) {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	w.WriteHeader(status)
 	json.NewEncoder(w).Encode(entry)
-}
-
-//FieldsExist ...
-func FieldsExist(s interface{}) {
-	v := reflect.ValueOf(s)
-	if v.Kind() == reflect.Ptr {
-		v = v.Elem()
-	}
-	t := v.Type()
-	for i := 0; i < t.NumField(); i++ {
-		sf := t.Field(i)
-		vf := v.Field(i)
-		switch sf.Type.Kind() {
-		case reflect.Chan, reflect.Func, reflect.Interface, reflect.Map, reflect.Ptr, reflect.Slice:
-			// if vf.IsNil() {
-			// 	log.Println(sf.Name)
-			// }
-			break
-		case reflect.Struct:
-			//log.Printf(sf.Name)
-			FieldsExist(vf.Interface())
-			break
-		default:
-			if vf.Interface() != reflect.Zero(sf.Type).Interface() {
-				if vf == reflect.Zero(sf.Type) {
-					//log.Println(sf.Name)
-				}
-				log.Println(sf.Name)
-			}
-			//log.Println(sf.Name)
-			//log.Println(vf)
-
-			//log.Println(reflect.Zero(sf.Type))
-			//log.Println(vf.Interface())
-		}
-	}
 }
