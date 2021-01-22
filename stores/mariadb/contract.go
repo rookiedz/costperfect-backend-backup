fb8c14ddaaabc4a9e3f6b4fc91895c2a607e01ed
sarawuth pimsai <sarawuth.pimsai@gmail.com>
2021-01-22 13:33:47 +0700

    Contract model

--- stores/mariadb/contract.go
+++ stores/mariadb/contract.go
@@ -1,15 +1,20 @@
 package mariadb
 
 import (
+	"context"
 	"costperfect/backend/models"
+	"database/sql"
+	"fmt"
 	"strings"
+	"time"
 )
 
 //Contract ...
 type Contract struct {
-	TableName   string
-	Columns     []string
-	QueryColumn string
+	TableName    string
+	Columns      []string
+	InsertColumn string
+	QueryColumn  string
 }
 
 //NewContract ...
@@ -36,12 +41,10 @@ func NewContract() Contract {
 		"contract_payment_percentage",
 		"contract_payment_amout",
 		"contract_payment_installments",
-		"contract_payment_installmentItems",
 		"contract_advance_payment_method",
 		"contract_advance_payment_percentage",
 		"contract_advance_payment_amout",
 		"contract_advance_payment_installments",
-		"contract_advance_payment_installment_items",
 		"contract_deduct_method",
 		"contract_deduct_percentage",
 		"contract_warranty_method",
@@ -50,38 +53,156 @@ func NewContract() Contract {
 		"contract_retention_money_method",
 		"contract_retention_money_percentage",
 		"contract_note",
-		"contract_attachments",
 	}
-	return Contract{TableName: "contracts", Columns: columns, QueryColumn: strings.Join(columns[:], ",")}
+	return Contract{TableName: "contracts", Columns: columns, InsertColumn: strings.Join(columns[1:], ","), QueryColumn: strings.Join(columns[:], ",")}
 }
 
 //Create ...
 func (c Contract) Create(contract models.Contract) (int64, error) {
-	return 0, nil
+	var ctx context.Context
+	var cancel context.CancelFunc
+	var stmt *sql.Stmt
+	var res sql.Result
+	var err error
+	var lastID int64
+	var cds string
+
+	ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	stmt, err = db.PrepareContext(ctx, fmt.Sprintf(`INSERT INTO %s (%s, contract_created_at, contract_updated_at)VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`, c.TableName, c.InsertColumn))
+	if err != nil {
+		return lastID, nil
+	}
+	defer stmt.Close()
+
+	cds = CurrentDatetimeString()
+	res, err = stmt.ExecContext(ctx, contract.ProjectID, contract.ContractorID, contract.EmployerID,
+		contract.Name, contract.ContractNo, contract.LetterOfIntentNo, contract.Value,
+		contract.Tax, contract.TaxValue, contract.NetValue, contract.SigningDate,
+		contract.BeginDate, contract.EndDate, contract.DeliveryDate, contract.WarrantyDays,
+		contract.PaymentMethod, contract.PaymentPercentage, contract.PaymentAmout,
+		contract.PaymentInstallments, contract.AdvancePaymentMethod, contract.AdvancePaymentPercentage,
+		contract.AdvancePaymentAmout, contract.AdvancePaymentInstallments, contract.DeductMethod,
+		contract.DeductPercentage, contract.WarrantyMethod, contract.WarrantyPercentage,
+		contract.PerformanceBondPercentage, contract.RetentionMoneyMethod, contract.RetentionMoneyPercentage,
+		contract.Note, cds, cds)
+	if err != nil {
+		return 0, err
+	}
+	lastID, err = res.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+	return lastID, nil
 }
 
 //Update ...
 func (c Contract) Update(id int64, contract models.Contract) error {
+	var ctx context.Context
+	var cancel context.CancelFunc
+	var stmt *sql.Stmt
+	var err error
+	var cds string
+	ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	stmt, err = db.PrepareContext(ctx, fmt.Sprintf(`UPDATE %s SET WHERE contract_id = ?`, c.TableName))
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	cds = CurrentDatetimeString()
+	if _, err = stmt.ExecContext(ctx); err != nil {
+		return err
+	}
 	return nil
 }
 
 //Delete ...
 func (c Contract) Delete(id int64) error {
+	var ctx context.Context
+	var cancel context.CancelFunc
+	var stmt *sql.Stmt
+	var err error
+	ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	stmt, err = db.PrepareContext(ctx, fmt.Sprintf(`DELETE FROM %s WHERE contract_id = ?`, c.TableName))
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	if _, err = stmt.ExecContext(ctx, id); err != nil {
+		return err
+	}
 	return nil
 }
 
 //DeleteByIDs ...
 func (c Contract) DeleteByIDs(ids []int64) error {
+	var ctx context.Context
+	var cancel context.CancelFunc
+	var stmt *sql.Stmt
+	var idsString string
+	var err error
+
+	idsString = ArrayInt64ToString(ids, ",")
+	ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	stmt, err = db.PrepareContext(ctx, fmt.Sprintf(`DELETE FROM %s WHERE contract_id IN (%s)`, c.TableName, idsString))
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	if _, err = stmt.ExecContext(ctx); err != nil {
+		return err
+	}
 	return nil
 }
 
 //DeleteByProject ...
 func (c Contract) DeleteByProject(id int64) error {
+	var ctx context.Context
+	var cancel context.CancelFunc
+	var stmt *sql.Stmt
+	var err error
+
+	ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	stmt, err = db.PrepareContext(ctx, fmt.Sprintf(`DELETE FROM %s WHERE project_id = ?`, c.TableName))
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	if _, err = stmt.ExecContext(ctx, id); err != nil {
+		return err
+	}
 	return nil
 }
 
 //DeleteByProjectIDs ...
 func (c Contract) DeleteByProjectIDs(ids []int64) error {
+	var ctx context.Context
+	var cancel context.CancelFunc
+	var stmt *sql.Stmt
+	var err error
+	var idsString string
+
+	idsString = ArrayInt64ToString(ids, ",")
+
+	ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	stmt, err = db.PrepareContext(ctx, fmt.Sprintf(`DELETE FROM %s WHERE project_id IN (%s)`, c.TableName, idsString))
+	if err != nil {
+		return err
+	}
+	if _, err = stmt.ExecContext(ctx); err != nil {
+		return err
+	}
 	return nil
 }
 
